259ed35d8fa571801cb181fa58a2b2d805b0fcf6
Phia(가영) <76558220+rkdud007@users.noreply.github.com>
2022-07-30 23:42:10 +0900

    connect wallet

--- griffin_front/pages/employee.js
+++ griffin_front/pages/employee.js
@@ -2,6 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
-  return <div className={styles.container}>Dashddboard</div>;
+export default function Employee() {
+  return <div className={styles.container}>Employee</div>;
 }
