0e581f64271f112f67b3da944e2b221095b70a10
Phia(가영) <76558220+rkdud007@users.noreply.github.com>
2022-08-07 08:17:21 +0900

    update-recent

--- griffin_front/components/profile.jsx
+++ griffin_front/components/profile.jsx
@@ -1,14 +1,27 @@
-import styles from "../styles/navbar.module.css";
+import styles from "../styles/profile.module.css";
 import { Descriptions } from "antd";
 import "antd/dist/antd.css";
 
 export default function Profile() {
+  const data = {
+    name: "GRIFFIN",
+    description: "make crypto payroll private",
+    email: "griffinpay2022@gmail.com",
+    wallet: "0xagw2332aawegew32r",
+    id: "2020170365",
+  };
+
   return (
-    <Descriptions title="Info">
-      <Descriptions.Item label="Name">Zhou Maomao</Descriptions.Item>
-      <Descriptions.Item label="Description">hihihihih</Descriptions.Item>
-      <Descriptions.Item label="Wallet">0xasegeegweg</Descriptions.Item>
-      <Descriptions.Item label="Email">dkdgk@agekgwe</Descriptions.Item>
-    </Descriptions>
+    <div className={styles.profileBox}>
+      <Descriptions title="Info">
+        <Descriptions.Item label="Name">{data.name}</Descriptions.Item>
+        <Descriptions.Item label="Description">
+          {data.description}
+        </Descriptions.Item>
+        <Descriptions.Item label="Email">{data.email}</Descriptions.Item>
+        <Descriptions.Item label="ID">{data.id}</Descriptions.Item>
+        <Descriptions.Item label="Wallet">{data.wallet}</Descriptions.Item>
+      </Descriptions>
+    </div>
   );
 }
--- griffin_front/components/states/employee-data.jsx
+++ griffin_front/components/states/employee-data.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 import { getEmployeeApi } from "../../api/employee";
 import _ from "lodash";
 
@@ -8,27 +9,30 @@ export const mapEmployeeData = (employee) => {
   const i = _.random(1, 2);
   const avatarUrl = `/pf${i}.png`;
 
-  return {...employee, avatarUrl}
-}
-
+  return { ...employee, avatarUrl };
+};
 
 export const EmployeeDataProvider = ({ children }) => {
   const [employeeReList, setEmployeeReList] = useState([]);
   const [employeeNReList, setEmployeeNReList] = useState([]);
   const [totalEmplyeeList, setTotalEmployeeList] = useState([]);
   const [totalEmployee, setTotalEmployee] = useState();
+  const router = useRouter();
+  const { employerId } = router.query;
   const getEmployee = async (employerId) => {
     console.log(employerId);
     const res = await getEmployeeApi(employerId);
     setEmployeeNReList(res.data.repeat_false.map(mapEmployeeData));
     setEmployeeReList((res.data.repeat_true || []).map(mapEmployeeData));
     setTotalEmployee(res.data.total_length);
-    setTotalEmployeeList((res.data.repeat_true || []).concat(res.data.repeat_false));
+    setTotalEmployeeList(
+      (res.data.repeat_true || []).concat(res.data.repeat_false)
+    );
     console.log(res);
   };
 
   useEffect(() => {
-    getEmployee("1");
+    getEmployee(employerId);
   }, []);
 
   return (
